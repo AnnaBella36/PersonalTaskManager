eb6ee1308cb1a75b4a6bc6f69c6cdca0958d4dfe
Barinova Olga <assistancebycompany0.1@gmail.com>
2025-07-07 21:45:40 -0500

    added empty state

--- PersonalTaskManager/EmptyStateView.swift
+++ PersonalTaskManager/EmptyStateView.swift
@@ -5,4 +5,34 @@
 //  Created by Olga Dragon on 07.07.2025.
 //
 
-import Foundation
+import UIKit
+
+final class EmptyStateView: UIView{
+    
+    private let messageLabel: UILabel = {
+        let label = UILabel()
+        label.text = "No Tasks"
+        label.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+        label.textAlignment = .center
+        label.textColor = .gray
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .clear
+        addSubview(messageLabel)
+        NSLayoutConstraint.activate([
+            messageLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            messageLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            messageLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            messageLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
